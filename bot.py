b34d43963d79815590d3c449ae923726ad79f30f
Florence <missflorencemary@hotmail.com>
2022-05-24 20:34:17 +0200

    Clean up code, add message sending functionality, react with a specific emoji on a specific word (case-insensitive)

--- bot.py
+++ bot.py
@@ -1,19 +1,32 @@
-import discord
-from discord.ext    import commands
-from discord.ext.commands   import Bot
-import asyncio
- 
-bot = commands.Bot(command_prefix='whatever_prefix_u_want_here')
- 
-@bot.event
+from discord.ext import commands
+from discord.utils import get
+
+my_bot = commands.Bot(command_prefix='Voldemort')
+
+
+@my_bot.event
 async def on_ready():
-    print ("whatever_text_u_want_here")
- 
- 
-@bot.event
+    print("Voldemort bot is up and running")
+
+
+@my_bot.event
 async def on_message(message):
-    if(message.channel.id == "channel_id_here"):
-        await bot.add_reaction(message, "discord_emote_id_here")
- 
- 
-bot.run("bot_token_here")
+    await react_to_message(message, "voldemort", "voldemort_emoji")
+    # await send_message(message, "I see what you're doing")
+
+
+async def react_to_message(message, search_term, emote_name):
+    if search_term in message.content.lower():
+        emoji = get(my_bot.emojis, name=emote_name)
+        await message.add_reaction(emoji=emoji)
+
+
+async def send_message(message, new_message):
+    # avoid an infinite loop if the bot posts messages
+    if message.author == my_bot.user:
+        return
+    # send a message in the same channel
+    await message.channel.send(new_message)
+
+
+my_bot.run("APPLICATION_BOT_TOKEN")
